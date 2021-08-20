c3fc2cbaa08242fd895ce21d42fbe7e594a57f4b
kintama48 <65843525+kintama48@users.noreply.github.com>
2021-08-21 01:15:01 +0500

    Update discord_to_sheets.py

--- cogs/discord_to_sheets.py
+++ cogs/discord_to_sheets.py
@@ -43,41 +43,46 @@ class DiscordToSheets(commands.Cog, name="sheet"):
                 self.sheets_id = config["sheets_id"]
                 file.close()
 
-#     # set's the bot's google sheet link
-#     @commands.command(name="resetsheet",
-#                       description="Adds a new Google Sheet (Syntax: `!resetsheet (new sheet's link)`)")
-#     @commands.has_role("Active Player")
-#     async def reset_sheet(self, context, link):
-#         id = link.strip().split("/")[3]
-#         if not os.path.isfile("config.json"):
-#             sys.exit("'config.json' not found! Add it and try again.")
-#         else:
-#             with open("config.json") as file:
-#                 config = json.load(file)
-#                 file.close()
-#         config["sheets_id"] = id
-#         with open("config.json", "w") as file:
-#             json.dump(config, file)
-#             file.close()
-#         self.sheets_id = id
-#         embed = discord.Embed(description="**`Sheet updated!`**", color=0x0a8a14)
-#         await context.send(embed=embed)
+    # # set's the bot's google sheet link
+    # @commands.command(name="resetsheet",
+    #                   description="Adds a new Google Sheet (Syntax: `!resetsheet (new sheet's link)`)")
+    # @commands.has_role("Active Player")
+    # async def reset_sheet(self, context, link):
+    #     id = link.strip().split("/")[3]
+    #     if not os.path.isfile("config.json"):
+    #         sys.exit("'config.json' not found! Add it and try again.")
+    #     else:
+    #         with open("config.json") as file:
+    #             config = json.load(file)
+    #             file.close()
+    #     config["sheets_id"] = id
+    #     with open("config.json", "w") as file:
+    #         json.dump(config, file)
+    #         file.close()
+    #     self.sheets_id = id
+    #     embed = discord.Embed(description="`**Sheet updated!**`", color=0x0a8a14)
+    #     await context.send(embed=embed)
 
     # records data on sheet
     @commands.command(name="record", description="Records the provided information on Google Sheets (Syntax: !record "
                                                  "{data})")
     @commands.has_role("Active Player")
     async def record(self, context: commands.Context, content_to_write):
-
         row_users = self.sheet.get(spreadsheetId=self.sheets_id, range="1:1").execute()  # ['values'][0]
         if 'values' in row_users:
             row_users = row_users['values'][0]
         else:
             row_users = []
 
+        column_days = self.sheet.get(spreadsheetId=self.sheets_id, range="A:A").execute()
+        if 'values' in column_days:
+            column_days = flatten(column_days['values'])
+        else:
+            column_days = []
+
         if row_users:
             if str(context.message.author) not in row_users:
-                range_column_id = convert_num_to_letters(len(row_users))
+                range_column_id = convert_num_to_letters(len(row_users) + 1)
                 self.write_to_sheet(range=f"{range_column_id}:{range_column_id}", content=str(context.message.author),
                                     append=True)
             else:
@@ -85,12 +90,6 @@ class DiscordToSheets(commands.Cog, name="sheet"):
         else:
             range_column_id = "B"
             self.write_to_sheet(range="B1", content=str(context.message.author))
-
-        column_days = self.sheet.get(spreadsheetId=self.sheets_id, range="A:A").execute()
-        if 'values' in column_days:
-            column_days = flatten(column_days['values'])
-        else:
-            column_days = []
 
         if column_days:
             msg_date = str(context.message.created_at).split()[0].strip()
@@ -134,9 +133,6 @@ def index_to_range(index):
     return chr(64 + index)
 
 
-def flatten(t):
-    return [item for sublist in t for item in sublist]
-
 def convert_num_to_letters(last_index):
     if last_index:
         index = last_index % 26
@@ -153,7 +149,10 @@ def convert_num_to_letters(last_index):
     else:
         sys.exit("Can't convert 0 to a letter")
 
+
+def flatten(t):
+    return [item for sublist in t for item in sublist]
+
+
 def setup(bot):
     bot.add_cog(DiscordToSheets(bot=bot))
-
-
