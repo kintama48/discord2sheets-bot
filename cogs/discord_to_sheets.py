c507622d74f1a7c63b0614b7c3971c6e7d1d5614
kintama48 <65843525+kintama48@users.noreply.github.com>
2021-08-21 01:08:13 +0500

    Update discord_to_sheets.py

--- cogs/discord_to_sheets.py
+++ cogs/discord_to_sheets.py
@@ -64,25 +64,20 @@ class DiscordToSheets(commands.Cog, name="sheet"):
 #         await context.send(embed=embed)
 
     # records data on sheet
-    @commands.command(name="record", description="Records the provided information on Google Sheets (Syntax: !record "
+        @commands.command(name="record", description="Records the provided information on Google Sheets (Syntax: !record "
                                                  "{data})")
     @commands.has_role("Active Player")
     async def record(self, context: commands.Context, content_to_write):
+
         row_users = self.sheet.get(spreadsheetId=self.sheets_id, range="1:1").execute()  # ['values'][0]
         if 'values' in row_users:
             row_users = row_users['values'][0]
         else:
             row_users = []
 
-        column_days = self.sheet.get(spreadsheetId=self.sheets_id, range="A:A").execute()
-        if 'values' in column_days:
-            column_days = flatten(column_days['values'])
-        else:
-            column_days = []
-
         if row_users:
             if str(context.message.author) not in row_users:
-                range_column_id = convert_num_to_letters(len(row_users) + 1)
+                range_column_id = convert_num_to_letters(len(row_users))
                 self.write_to_sheet(range=f"{range_column_id}:{range_column_id}", content=str(context.message.author),
                                     append=True)
             else:
@@ -90,6 +85,12 @@ class DiscordToSheets(commands.Cog, name="sheet"):
         else:
             range_column_id = "B"
             self.write_to_sheet(range="B1", content=str(context.message.author))
+
+        column_days = self.sheet.get(spreadsheetId=self.sheets_id, range="A:A").execute()
+        if 'values' in column_days:
+            column_days = flatten(column_days['values'])
+        else:
+            column_days = []
 
         if column_days:
             msg_date = str(context.message.created_at).split()[0].strip()
@@ -124,9 +125,9 @@ class DiscordToSheets(commands.Cog, name="sheet"):
                               valueInputOption='USER_ENTERED', body=body).execute()
 
 
-def convert_num_to_letters(last_index):
-    f = lambda x: sys.exit("Can't convert 0 to a letter") if x == 0 else f((x - 1) // 26) + chr((x - 1) % 26 + ord("A"))
-    return f(last_index)
+# def convert_num_to_letters(last_index):
+#     f = lambda x: sys.exit("Can't convert 0 to a letter") if x == 0 else f((x - 1) // 26) + chr((x - 1) % 26 + ord("A"))
+#     return f(last_index)
 
 
 def index_to_range(index):
@@ -136,25 +137,23 @@ def index_to_range(index):
 def flatten(t):
     return [item for sublist in t for item in sublist]
 
+def convert_num_to_letters(last_index):
+    if last_index:
+        index = last_index % 26
+        count = (last_index // 26)
+        if not index:
+            index = 26
+        if last_index % 26 == 0:
+            count -= 1
+        if count:
+            range_id = f"{index_to_range(count)}{index_to_range(index)}"
+        else:
+            range_id = index_to_range(index)
+        return range_id
+    else:
+        sys.exit("Can't convert 0 to a letter")
 
 def setup(bot):
     bot.add_cog(DiscordToSheets(bot=bot))
 
-# def convert_num_to_letters(last_index):
-#     if last_index:
-#         index = last_index % 26
-#         count = (last_index // 26)
-#         if not index:
-#             index = 26
-#         if last_index % 26 == 0:
-#             count -= 1
-#         if count:
-#             range_id = f"{index_to_range(count)}{index_to_range(index)}"
-#         else:
-#             range_id = index_to_range(index)
-#         return range_id
-#     else:
-#         sys.exit("Can't convert 0 to a letter")
-#
-#
 
